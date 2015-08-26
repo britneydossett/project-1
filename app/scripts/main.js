67a95893b8a987decc5d0c62e18ef4243859e2e7
Britney Dossett <britneydossett@gmail.com>
2015-08-25 22:52:49 -0400

    Added new squirrel

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -114,36 +114,135 @@ function step() {
     acorn = $(acorn);
     if (fallingAcorn(acorn)) {
       updateAcorn(acorn);
+        onUpdate: detectAcornCollision;
       // console.log('acorn: bottom=' + acorn.offset().bottom + ', class=' + acorn.attr('class'));
 
       // TODO: check for a caught acorn (collision detection)
 }
   });
 }
+// function collision(acorn, basket){
+//     var hw = basket.style.width >> 1; // half width of object
+//     var hh = basket.style.height >> 1; // (bit shift is faster than / 2)
+//     var cx = basket.style.left + hw; // centre point in x
+//     var cy = basket.style.top + hh; // and in y
 
-function collision($basket, $acorn) {
-      var x1 = $basket.offset().left;
-      var y1 = $basket.offset().top;
-      var h1 = $basket.outerHeight(true);
-      var w1 = $basket.outerWidth(true);
-      var b1 = y1 + h1;
-      var r1 = x1 + w1;
-      var x2 = $acorn.offset().left;
-      var y2 = $acorn.offset().top;
-      var h2 = $acorn.outerHeight(true);
-      var w2 = $acorn.outerWidth(true);
-      var b2 = y2 + h2;
-      var r2 = x2 + w2;
+//     var hw1 = acorn.style.width >> 1; // half width of object
+//     var hh1 = acorn.style.height >> 1; // (bit shift is faster than / 2)
+//     var cx1 = acorn.style.left + hw1; // centre point in x
+//     var cy1 = acorn.style.top + hh1; // and in y
 
-      if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) return numCaught;
-      return ++numCaught;
-    }
+//     var xDif = Math.abs(cx - cx1); // where cx1 is centre of object to check against
+
+//     if(xDif > hw + hw1) return false; // there is no possibility of a collision!
+
+//     var yDif = Math.abs(cy - cy1);
+
+//     if(yDif > hh + hh1) return false; // no collision - bug out.
+//     else {
+//       numCaught++
+// }
+// }
+
+// function doObjectsCollide(acorn, basket) { // a and b are your objects
+//     // console.log(acorn.offset().top,acorn.position().top, basket.position().top, acorn.width(),acorn.height(), basket.width(),basket.height());
+//     var aTop = acorn.offset().top;
+//     var aLeft = acorn.offset().left;
+//     var bTop = basket.offset().top;
+//     var bLeft = basket.offset().left;
+
+//     return !(
+//         ((aTop + acorn.height()) < (bTop)) ||
+//         (aTop > (bTop + basket.height())) ||
+//         ((aLeft + acorn.width()) < bLeft) ||
+//         (aLeft > (bLeft + basket.width()))
+//     );
+// }
+
+// function doObjectsCollide(acorn, basket) { // a and b are your objects
+//    return !(
+//     ((acorn.getY() + acorn.getHeight()) < (basket.getY())) ||
+//     (acorn.getY() > (basket.y() + basket.getHeight())) ||
+//     ((acorn.getX() + acorn.getWidth()) < basket.getX()) ||
+//     (acorn.getX() > (basket.getX() + basket.getWidth()))
+//    );
+//    if (caughtAcorns === true) {
+//       numCaught++;
+//     }
+// }
+
+function detectAcornCollision($acorn){
+    $("#basket").each(function(index){
+        var $e = $(this);
+        if(!(
+            $e.position().left > ($acorn.position().left + $acorn.width())
+         || ($e.position().left + $e.width()) < $acorn.position().left
+         || $e.position().top > ($acorn.position().top + $acorn.height())
+         || ($e.position().top + $e.height()) < $acorn.position().top)
+        ){
+            console.log('collision');
+        }
+    });
+}
+
+// function collides(acorn, basket) {
+//     var acorn.height = 60;
+//     var acorn.width = 60;
+//     var acorn.x = acorn.offset().left
+//     var acorn.y = acorn.offset().top
+
+//     var basket.height = 90;
+//     var basket.width = 200;
+//     var basket.x = acorn.offset().left
+//     var basket.y = acorn.offset().top
+
+//   return acorn.x < basket.x + basket.width &&
+//          acorn.x + acorn.width > basket.x &&
+//          acorn.y < basket.y + basket.height &&
+//          acorn.y + acorn.height > basket.y;
+// }
+// function handleCollisions() {
+//   fallingAcorn.forEach(function(acorn) {
+//     basket.forEach(function(basket) {
+//       if (collides(acorn, basket)) {
+//         basket.keepScore();
+//         acorn.active = false;
+//       }
+//     });
+//   });
+// }
+// function collision($basket, $acorn) {
+//       var x1 = $basket.offset().left;
+//       var y1 = $basket.offset().top;
+//       var h1 = $basket.outerHeight(true);
+//       var w1 = $basket.outerWidth(true);
+//       var b1 = y1 + h1;
+//       var r1 = x1 + w1;
+//       var x2 = $acorn.offset().left;
+//       var y2 = $acorn.offset().top;
+//       var h2 = $acorn.outerHeight(true);
+//       var w2 = $acorn.outerWidth(true);
+//       var b2 = y2 + h2;
+//       var r2 = x2 + w2;
+
+//       if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) {
+//         numCaught++;
+      // }
+      // else {
+      //   return keepScore(numCaught++);
+//       }
+// }
+// function collision(){
+//     var caughtAcorns = $('#basket').collision('.acorn');
+
+//     if (caughtAcorns === true) {
+//       numCaught++;
+//     }
+// }
+// collision();
 
 // once page loads, call fallingAcorn function
 $(function() {
-  setInterval(function() {
-    $('keepScore').text(collision($('#basket'), $('.acorn')));
-  }, 100);
   setInterval(step, GAME_SPEED);
   setInterval(fallingAcornManager, ACORN_MANAGER_SPEED);
 });
